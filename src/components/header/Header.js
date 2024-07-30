17874f42d5f29e4e9f96c785740c2f66d1b31d4d
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-07-30 14:36:30 +0500

    react start

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css"
+
+
+function Header() {
+    return (
+        <div className="header">
+                sad
+        </div>
+    )
+}
+
+export default Header
